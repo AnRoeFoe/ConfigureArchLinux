fd8286e47332b217fc2733b37469195c42aa5a38
Andreas Röhrle-Förnzler <andreas.roehrle@web.de>
2026-06-07 16:48:08 +0200

    Add throttled, helvum, fastfetch and klick. Enable throttled and cronie.

--- src/ConfigureArchLinux.sh
+++ src/ConfigureArchLinux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman --noconfirm -Sy neovim base-devel stow less kitty starship ttf-firacode-nerd gufw zellij secrets
+sudo pacman --noconfirm -Sy neovim base-devel stow less kitty starship ttf-firacode-nerd gufw zellij secrets throttled helvum
 
 mkdir ~/AUR
 cd ~/AUR
@@ -8,7 +8,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg --noconfirm -si
 
-yay --noconfirm -Sy visual-studio-code-bin zen-browser-bin
+yay --noconfirm -Sy visual-studio-code-bin zen-browser-bin fastfetch klick
 
 git clone https://github.com/AnRoeFoe/dotfiles.git ~/.repos/dotfiles
 cd ~/.repos/dotfiles
@@ -21,3 +21,6 @@ stow -t ~ kitty
 stow -t ~ inputrc
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+sudo systemctl enable --now throttled.service
+sudo systemctl enable --now cronie.service
